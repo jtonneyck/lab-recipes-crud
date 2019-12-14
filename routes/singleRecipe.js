28e1a409f9316725182dcd2d6791065c5c5c7f35
Monika <mswidzinska87@gmail.com>
2019-12-14 23:44:49 +0100

    iteration 2

--- routes/singleRecipe.js
+++ routes/singleRecipe.js
@@ -4,18 +4,13 @@ const Recipe = require("../models/recipe")
 
 
 // QUERY METHOD
-app.get("/recipes/:id", (req, res) => {
-    Recipe.find({ id: req.query.id })
-        .then((result) => {
+app.get("/recipes:id", (req, res) => {
+    Recipe.findById(req.params.id)
+        .then(result => {
             debugger
-            for (let i = 0; i < result.length; i++) {
-                res.render("recipes/singleRecipe", { recipe: result[i] })
-                console.log(i)
-            }
+            res.render("recipes/singleRecipe", { recipe: result })
         })
         .catch(err => console.log(err))
 })
-
-
 
 module.exports = app
