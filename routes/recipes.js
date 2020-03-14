6c0d3a233aa00e8ce826682916e128e29a8c95bd
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-03-14 18:39:14 +0100

    Added styling

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const Recipe = require("../models/recipe");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   Recipe.find({})
-    .then(respond => {
-      res.render("recipes/list.hbs", { allRecipes: respond });
+    .then(allRecipes => {
+      res.render("recipes/list.hbs", { allRecipes: allRecipes });
     })
     .catch(err => {
       res.render("error", err);
@@ -19,33 +19,48 @@ app.get("/create", (req, res) => {
   res.render("recipes/create.hbs");
 });
 
-app.post("/create", (req,res) =>{
+app.post("/create", (req, res) => {
   console.log(req.body);
-  Recipe
-      .create({
-          title: req.body.title,
-          cuisine: req.body.cuisine,
-          duration: req.body.duration,
-          dishType: req.body.dishType,
-          level: req.body.level,
-          ingredients: req.body.ingredients,
-          image: req.body.image,
-      })
-      .then((newRecipe) =>{
-        res.redirect(`/recipes/${newRecipe.id}`)
-      })
-      .catch((error) =>{
-          res.send("error", error)
-      })
-})
+  Recipe.create({
+    title: req.body.title,
+    cuisine: req.body.cuisine,
+    duration: req.body.duration,
+    dishType: req.body.dishType,
+    level: req.body.level,
+    ingredients: req.body.ingredients,
+    image: req.body.image
+  })
+    .then(newRecipe => {
+      res.redirect(`/recipes/${newRecipe.id}`);
+    })
+    .catch(error => {
+      res.send("error", error);
+    });
+});
 
-//RECIPE DETAIL
-app.get("/:id", (req, res) => {
-  console.log("Rendering route");
+//UPDATE
+app.get("/:id/update", (req, res) => {
   Recipe.findById(req.params.id)
-    .then(respond => {
-      console.log("the response is", respond);
-      res.render("recipes/recipe-detail.hbs", { oneRecipe: respond });
+    .then(recipe => {
+      res.render("recipes/update.hbs", { oneRecipe: recipe });
+    })
+    .catch(err => {
+      res.render("error", err);
+    });
+});
+
+app.post("/:id/update", (req, res) => {
+  Recipe.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    cuisine: req.body.cuisine,
+    duration: req.body.duration,
+    dishType: req.body.dishType,
+    level: req.body.level,
+    ingredients: req.body.ingredients,
+    image: req.body.image
+  })
+    .then(recipe => {
+      res.redirect(`/recipes/${req.params.id}`);
     })
     .catch(err => {
       console.log("this is an error", err);
@@ -53,6 +68,39 @@ app.get("/:id", (req, res) => {
     });
 });
 
+// app.post("/update", (req,res) =>{
+//   console.log(req.body);
+//   Recipe
+//       .create({
+//           title: req.body.title,
+//           cuisine: req.body.cuisine,
+//           duration: req.body.duration,
+//           dishType: req.body.dishType,
+//           level: req.body.level,
+//           ingredients: req.body.ingredients,
+//           image: req.body.image,
+//       })
+//       .then((newRecipe) =>{
+//         res.redirect(`/recipes/${newRecipe.id}`)
+//       })
+//       .catch((error) =>{
+//           res.send("error", error)
+//       })
+// })
+
+//RECIPE DETAIL
+app.get("/:id", (req, res) => {
+  console.log("Rendering route");
+  Recipe.findById(req.params.id)
+    .then(recipe => {
+      console.log("the response is", recipe);
+      res.render("recipes/recipe-detail.hbs", { oneRecipe: recipe });
+    })
+    .catch(err => {
+      console.log("this is an error", err);
+      res.send("error", err);
+    });
+});
 
 //DELETE
 
@@ -67,13 +115,11 @@ app.get("/:id/delete", (req, res) => {
     });
 });
 
+//to test delete without deleting
 // app.get("/:id/delete", (req, res) => {
 //   console.log(req);
 //   console.log(`The recipe ${req.params.id} will be deleted`);
 //   res.redirect("/recipes");
 // });
-
-
-
 
 module.exports = app;
