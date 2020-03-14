61a33cd453d19591754da78fb96f94a51a907bd8
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-03-14 13:09:39 +0100

    Initial commit

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Recipe = mongoose.model("recipes", {
-    //...
-})
+const recipeSchema = new Schema({title: String,
+    level: String,
+    ingredients: Array,
+    cuisine: String,
+    dishType: String,
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+      type: Number,
+      min: 0
+    },
+    creator: String,
+    created: {
+      type: Date,
+      default: Date.now
+    }
+});
+
+const Recipe = mongoose.model("recipes", recipeSchema);
 
 module.exports = Recipe;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,48 @@ const express = require("express");
 const app = express();
 const Recipe = require("../models/recipe");
 
-app.get("/", (req,res)=> {
-    Recipe.find({})
-        .then((recipes)=> {
-            res.render("recipes/list", recipes);
-        })
-        .catch((err)=> {
-            res.render("error", err);
-        })
-})
+app.get("/", (req, res) => {
+  Recipe.find({})
+    .then(respond => {
+      res.render("recipes/list.hbs", { allRecipes: respond });
+    })
+    .catch(err => {
+      res.render("error", err);
+    });
+});
+
+app.get("/:id", (req, res) => {
+  console.log("Rendering route");
+  Recipe.findById(req.params.id)
+    .then(respond => {
+      console.log("the response is", respond);
+      res.render("recipes/recipe-detail.hbs", { oneRecipe: respond });
+    })
+    .catch(err => {
+      console.log("this is an error", err);
+      res.send("error", err);
+    });
+});
+
+// app.get("/recipe/:id/delete", (req, res) => {
+//   Recipe.findByIdAndDelete(req.params.id)
+//     .then(recipe => {
+//       res.redirect("/recipes");
+//     })
+//     .catch(err => {
+//       console.log("this is an error", err);
+//       res.send("error", err);
+//     });
+// });
+
+app.get("/:id/delete", (req, res) => {
+  console.log(req);
+  console.log(`The recipe ${req.params.id} will be deleted`);
+  res.redirect("/recipes");
+});
+
+app.get("/create", (req, res) => {
+  res.render("recipes/create.hbs");
+});
+
+module.exports = app;
