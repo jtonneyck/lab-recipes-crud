0d24131f7c084bfb5a7237e80db2f79b946afac9
Elena Perrone <elenperrone11@gmail.com>
2020-03-15 15:00:13 +0100

    iteration1

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const hbs = require("hbs")
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
 const app = express();
 
-// set up handlebars
 
-app.use("/recipes", require("./routes/recipes"));
+mongoose.connect('mongodb://localhost/recipesDb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=> {
+    console.log("Connected to database");
+})
+.catch((error)=> {
+    console.log("Not connected to database, error:", error);
+})
 
-app.listen(3000, ()=> {
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'hbs');
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(express.static(__dirname + '/public'));
+
+app.use("/", require("./routes/recipes"));
+
+
+app.listen(3000, () => {
     console.log("Webserver is listening");
 })
--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const Recipe = mongoose.model("recipes", {
-    //...
+const recipeModel = new Schema ({
+    title: String,
+    cuisine: { type: String, required: true }, // is this who we must write this?
+    image: { type: String, default: 'https: //images.media-allrecipes.com/images/75131.jpg' }
 })
 
+const Recipe = mongoose.model("recipes", recipeModel);
+
 module.exports = Recipe;
+
+
+
+
+
+
