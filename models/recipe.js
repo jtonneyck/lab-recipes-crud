61044e91e739ae1992a8da9fe16211c14e98ee41
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-15 03:59:29 +0100

    all done except update function

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema
 
-const Recipe = mongoose.model("recipes", {
-    //...
+const recipeSchema = new Schema({
+    title:String,
+    ingredients:[String],
+    level:String,
+    creator:String,
+    cuisine:String,
+    image:String,
+    duration:Number
 })
+
+const Recipe = mongoose.model("recipes", recipeSchema)
 
 module.exports = Recipe;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,32 @@ const express = require("express");
 const app = express();
 const Recipe = require("../models/recipe");
 
-app.get("/", (req,res)=> {
-    Recipe.find({})
-        .then((recipes)=> {
-            res.render("recipes/list", recipes);
+app.get("/recipes", (req,res)=> {
+    Recipe.find()
+        .then((recipeData)=> {
+            res.render("recipes", {recipes:recipeData});
         })
         .catch((err)=> {
             res.render("error", err);
         })
 })
+
+app.post('/recipes', (req,res)=>{
+    console.log(req.body)
+    Recipe
+        .create({
+            title: req.body.title,
+            cuisine: req.body.cuisine,
+            creator: req.body.creator,
+            duration: req.body.duration
+        })
+        .then(()=> {
+            res.redirect('/recipes');
+        })
+        .catch((err)=> {
+            res.send("error");
+        })
+})
+
+
+module.exports = app
