8b9d28ca4d4fe7ec800312dcce508a08beefbbc5
unknown <ahabey7@gmail.com>
2020-03-18 15:01:10 +0100

    iteration1

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,12 @@
-const mongoose = require("mongoose");
-
-const Recipe = mongoose.model("recipes", {
-    //...
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const recipeSchema = new Schema({
+    title: String,
+    cuisine: String,
+    image: String
 })
+
+
+const Recipe = mongoose.model("recipe", recipeSchema)
 
 module.exports = Recipe;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const Recipe = require("../models/recipe");
 
-app.get("/", (req,res)=> {
-    Recipe.find({})
-        .then((recipes)=> {
-            res.render("recipes/list", recipes);
+app.get("/recipes", (req,res)=> {
+    Recipe.find()
+        .then((recipeData)=> {
+            res.render("recipes", {recipes:recipeData});
         })
         .catch((err)=> {
             res.render("error", err);
         })
 })
+
+module.exports = app;
