229a54585290ce7952c456b72463110201a79914
“Alexandra <alex.muskovszky@gmail.com>
2020-03-18 12:18:47 +0100

    “done”

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
-// set up handlebars
+// hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.engine('html', require('hbs').__express);
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/recipes", require("./routes/recipes"));
+//route
+app.use("/", require("./routes/recipes"));
 
-app.listen(3000, ()=> {
+//DB connection
+mongoose.connect('mongodb://localhost/recipe-app-dev', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((connectionInfo) => {
+        console.log("connected to recipe-app-dev DB");
+    })
+    .catch((error) => {
+        console.log("ERROR ERROR", error);
+    });
+
+//listener
+app.listen(3000, () => {
     console.log("Webserver is listening");
 })
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,96 @@ const express = require("express");
 const app = express();
 const Recipe = require("../models/recipe");
 
-app.get("/", (req,res)=> {
-    Recipe.find({})
-        .then((recipes)=> {
-            res.render("recipes/list", recipes);
+//1 list
+app.get("/recipes", (req, res) => {
+    Recipe
+        .find({})
+        .then((recipes) => {
+            res.render("recipes/list", {
+                recepiesHBS: recipes
+            });
         })
-        .catch((err)=> {
+        .catch((err) => {
             res.render("error", err);
         })
 })
+
+//2 detail
+app.get("/recipes/detail/:id", (req, res) => {
+    Recipe
+        .findById(req.params.id)
+        .then((recipeData) => {
+            res.render("recipes/detail", {
+                recipeHbs: recipeData
+            });
+        })
+        .catch((err) => {
+            res.send('error');
+        })
+})
+//3 delete
+app.get("/recipes/delete/:id", (req, res) => {
+    Recipe
+        .findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect("/recipes")
+        })
+        .catch((err) => {
+            res.send('error');
+        })
+})
+//4 create
+app.get('/recipes/create', (req, res) => {
+    res.render("recipes/create");
+});
+app.post('/recipes/create', (req, res) => {
+    Recipe
+        .create({
+            title: req.body.title,
+            cuisine: req.body.cuisine,
+            image: req.body.image,
+            level: req.body.level,
+            dishType: req.body.dishType,
+            duration: req.body.duration,
+            creator: req.body.creator
+        })
+        .then(() => {
+            res.redirect('/recipes');
+        })
+        .catch((err) => {
+            res.send('error');
+        })
+});
+//5 update
+app.get("/recipes/update/:id", (req, res) => {
+    Recipe
+        .findById(req.params.id)
+        .then((recipeData) => {
+            res.render("recipes/update", {
+                recipeHbs: recipeData
+            });
+        })
+        .catch((err) => {
+            res.send('error');
+        })
+})
+app.post("/recipes/update/:id", (req, res, next) => {
+    Recipe
+        .findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            cuisine: req.body.cuisine,
+            image: req.body.image,
+            level: req.body.level,
+            dishType: req.body.dishType,
+            duration: req.body.duration,
+            creator: req.body.creator
+        })
+        .then((recipeHbs) => {
+            res.redirect(`/recipes/detail/${recipeHbs._id}`);
+        })
+        .catch((err) => {
+            res.send('error');
+        })
+})
+
+module.exports = app
