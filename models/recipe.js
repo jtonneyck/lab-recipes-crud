a38da7633d9a669ccdecabefafc99c1cc17370ae
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-03-22 00:52:07 +0100

    done

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,68 @@
+//Setting up MONGOOSE
 const mongoose = require("mongoose");
 
-const Recipe = mongoose.model("recipes", {
-    //...
-})
+//Schema Definition
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  // title:String,    
+  // ingredients:[String],    
+  // level:String,    
+  // creator:String,    
+  // cuisine:String,    
+  // image:String,    
+  // duration:Number,
+  // file:
+    title: {
+      type: String, 
+      required: true, 
+      unique: true
+    },
+  
+    level: {
+      type: String,
+      enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+  
+    ingredients: {
+      type: Array
+    },
+  
+    cuisine: {
+      type: String, 
+      required: true
+    },
+  
+    dishType: {
+      type: String,
+      enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+    },
+  
+    image: {
+      type: String, 
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+  
+    duration: {
+      type: Number, 
+      min: 0
+    },
+  
+    creator: {
+      type: String
+    },
+  
+    created: {
+      type: Date, 
+      default: Date.now
+    },
+
+    file: {
+      type: String
+    }
+  });
+
+//MODEL EXPORT
+const Recipe = mongoose.model("recipes", recipeSchema);
 
 module.exports = Recipe;
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,13 +1,98 @@
 const express = require("express");
 const app = express();
-const Recipe = require("../models/recipe");
+const Recipe = require("../models/Recipe");
+const multer  = require('multer');
+const upload = multer({ dest: 'public/'});
 
-app.get("/", (req,res)=> {
-    Recipe.find({})
-        .then((recipes)=> {
-            res.render("recipes/list", recipes);
+//Page that contains all of the recipes
+app.get("/recipes", (request, response) => {
+    Recipe
+        .find()
+        .then((recipeData) => {
+            response.render('recipes', {recipeHbs: recipeData});
+        })
+        .catch((error) => {
+            response.send("error", error);
+        });
+});
+
+//Loads the detail page of your chosen recipe
+app.get('/recipes/detail/:recipeId', (request, response) => {
+    Recipe
+        .findById(request.params.recipeId)
+        .then((recipeData) => {
+            response.render('../views/detail', {recipeHbs: recipeData});
+        })
+        .catch((error) => {
+            response.send("error", error);
+        });
+});
+
+//You can delete your recipe
+app.get('/recipes/delete/:recipeId', (request, response) => {
+    Recipe
+        .findByIdAndDelete(request.params.recipeId)
+        .then((recipeData) => {
+            response.redirect("/recipes");
+        })
+        .catch((error) => {
+            response.send("error", error);
+        });
+});
+
+//Page where you can add your own recipes
+app.get("/recipes/create", (request,response)=> {
+    response.render("add");
+});
+
+app.post("/recipes/create", upload.single('preparationSteps'), (request,response)=> {
+    Recipe
+        .create({
+            title: request.body.title,
+            level: request.body.level,
+            cuisine: request.body.cuisine,
+            dishType: request.body.dishType,
+            duration: request.body.duration,
+            image: request.body.image,
+            file: request.file.filename
+        })
+        .then((recipeInfo)=> {
+            response.redirect(`/recipes`);
         })
         .catch((err)=> {
-            res.render("error", err);
+            response.send("error");
+        });
+});
+
+//Page where you can update the recipe
+app.get("/recipes/update/:recipeId", (request,response)=> {
+    Recipe
+        .findById(request.params.recipeId)
+        .then((recipeData)=> {
+            response.render("update", {recipeHbs: recipeData});
         })
-})
+        .catch((err)=> {
+            response.send("Error");
+        });
+});
+
+app.post("/recipes/update/:recipeId", (request,response)=> {
+    console.log(request.body)
+    Recipe
+        .findByIdAndUpdate(request.params.recipeId,{
+            title: request.body.title,
+            level: request.body.level,
+            cuisine: request.body.cuisine,
+            dishType: request.body.dishType,
+            duration: request.body.duration,
+            image: request.body.image
+        })
+        .then((recipeData)=> {
+            response.redirect(`/recipes/detail/${recipeData._id}`);
+        })
+        .catch((err)=> {
+            response.send("err");
+        });
+});
+
+module.exports = app;
